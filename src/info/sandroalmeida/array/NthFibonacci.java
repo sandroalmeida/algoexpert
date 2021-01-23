85f1953ce802f553a0929af18f9126639e4033fc
Sandro Almeida <sandro.almeida@dotbuild.com.br>
2021-01-22 23:00:20 -0800

    NthFibonacci implemented.

---
+++ src/info/sandroalmeida/array/NthFibonacci.java
@@ -0,0 +1,28 @@
+package info.sandroalmeida.array;
+
+public class NthFibonacci {
+
+    public static int getNthFib(int n) {
+
+        int n1 = 0, n2 = 1;
+        if(n == 1) return n1;
+        if(n == 2) return n2;
+
+        for(int i = 3; i <= n; i++){
+            int n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+        }
+
+        return n2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getNthFib(1));
+        System.out.println(getNthFib(2));
+        System.out.println(getNthFib(3));
+        System.out.println(getNthFib(4));
+        System.out.println(getNthFib(5));
+        System.out.println(getNthFib(6));
+    }
+}

d5ace4122876d8634feb95d12cb8a089524589b8
Sandro Almeida <sandro.almeida@dotbuild.com.br>
2021-01-23 16:16:42 -0800

    PalindromeCheck implemented.

---
+++ src/info/sandroalmeida/array/PalindromeCheck.java
@@ -0,0 +1,23 @@
+package info.sandroalmeida.array;
+
+public class PalindromeCheck {
+
+    public static boolean isPalindrome(String str) {
+        char[] chars = str.toCharArray();
+        int p1 = 0, p2 = chars.length - 1;
+        boolean check = true;
+
+        while(p1 < p2){
+            if(chars[p1] != chars[p2])
+                check = false;
+            p1++;
+            p2--;
+        }
+
+        return check;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("abcdcba"));
+    }
+}

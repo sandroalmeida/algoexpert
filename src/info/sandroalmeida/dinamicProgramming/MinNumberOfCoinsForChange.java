c62a839d50fc5e451c993bf6f715b705b42c11b0
sandroalmeida <sandrodotbuild@gmail.com>
2021-09-23 16:17:35 -0700

    MinNumberOfCoinsForChange implementation.

---
+++ src/info/sandroalmeida/dinamicProgramming/MinNumberOfCoinsForChange.java
@@ -0,0 +1,33 @@
+package info.sandroalmeida.dinamicProgramming;
+
+import java.util.Arrays;
+
+public class MinNumberOfCoinsForChange {
+
+    public static int minNumberOfCoinsForChange(int n, int[] denoms) {
+
+        int[] numOfCoins = new int[n + 1];
+        Arrays.fill(numOfCoins, Integer.MAX_VALUE);
+        numOfCoins[0] = 0;
+        int toCompare = 0;
+        for(int denom: denoms){
+            for(int amount = 0; amount < numOfCoins.length; amount++){
+                if(denom <= amount){
+                    if(numOfCoins[amount - denom] == Integer.MAX_VALUE){
+                        toCompare = numOfCoins[amount - denom];
+                    } else{
+                        toCompare = numOfCoins[amount - denom] + 1;
+                    }
+                    numOfCoins[amount] = Math.min(numOfCoins[amount], toCompare);
+                }
+            }
+        }
+        return numOfCoins[n] != Integer.MAX_VALUE ? numOfCoins[n] : -1;
+    }
+
+
+    public static void main(String[] args) {
+        int[] input = {3, 5};
+        System.out.println(minNumberOfCoinsForChange(9, input));
+    }
+}
